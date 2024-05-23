33a52c2ae68fc41ff8bf255849e87b1e66911f4a
hw-codekim <codekim@naver.com>
2024-05-24 00:05:38 +0900

    update_202405240005

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.page_link("pages/streamlit_app.py", label="Page 1", icon="1️⃣")
-st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣")
-st.page_link("http://www.naver.com", label="Google", icon="🌎")
+st.page_link("pages/streamlit_app.py", label="streamlit_app", icon="1️⃣")
+st.page_link("pages/financeChart.py", label="financeChart", icon="2️⃣")
+st.page_link("http://www.naver.com", label="naver", icon="🌎")
 
